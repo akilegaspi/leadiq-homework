bdef8e2df72b41ebcf501e9f0a18884dbd2c638c
Adrian Legaspi <aki.legaspi@gmail.com>
2016-08-25 18:32:07 +0800

    remove logger inside controller

--- app/controllers/TransactionController.scala
+++ app/controllers/TransactionController.scala
@@ -17,7 +17,6 @@ class TransactionController extends Controller {
 
   def insert(id: Long)= Action { req =>
     val json = req.body.asJson
-    Logger.info(json.toString)
     json.map{ body =>
       val transact = body.as[Transaction]
       transact.parentId match {
