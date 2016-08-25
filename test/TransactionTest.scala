cf890590a30b091148cafb026dfa138e19160a8e
Adrian Legaspi <aki.legaspi@gmail.com>
2016-08-25 13:01:32 +0800

    Create test case
    Create a test case that generates a random value 100 times passing it to the server URI

--- test/TransactionTest.scala
+++ test/TransactionTest.scala
@@ -1,9 +1,10 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+
+import scala.util.Random
 
 
 /**
@@ -13,16 +14,26 @@ import play.api.test.Helpers._
 class TransactionTest extends Specification {
 
   "TransactionService" should {
+    implicit val app = FakeApplication()
     val baseuri = "/transactionservice"
-    "send status ok" in new WithApplication {
-      (1 to 10).map( num => route(FakeRequest(PUT, s"$baseuri/transact/$num")))
+    "PUT transaction data JSON" in new WithApplication {
+      val header = FakeHeaders(List(("type", "application/json")))
+
+      val list = List("cars", "animals", "people")
+
+      def getType = list( new Random().nextInt(list.length - 1))
+
+      def dataGen(num: Long) = {
+        val rand = new Random().nextFloat * 1000
+          s"""{
+              |"amount" : $rand,
+              |"types"  : "$getType" ${if (new Random().nextBoolean) s", \"parent_id\": $num "}
+              |}""".stripMargin
+
+      }
+      (1 to 100).map( num => route(FakeRequest(PUT, s"$baseuri/transact/$num", header, dataGen(num))))
+      
     }
-
-    "send back data" in new WithApplication{
-      (1 to 10).map( num => route(FakeRequest(GET, s"$baseuri/transact/$num")))
-    }
-
-
 
   }
 
